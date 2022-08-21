a4b465c5bbe5095118f92e23a9fdad69359232f4
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-21 20:42:52 +0900

    refactor: no auth

--- worksheet/config/common.py
+++ worksheet/config/common.py
@@ -191,11 +191,11 @@ class Common(Configuration):
             'rest_framework.renderers.JSONRenderer',
             'rest_framework.renderers.BrowsableAPIRenderer',
         ),
-        'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
-        ],
-        'DEFAULT_AUTHENTICATION_CLASSES': (
-            'rest_framework.authentication.SessionAuthentication',
-            'rest_framework.authentication.TokenAuthentication',
-        )
+        # 'DEFAULT_PERMISSION_CLASSES': [
+        #     'rest_framework.permissions.IsAuthenticated',
+        # ],
+        # 'DEFAULT_AUTHENTICATION_CLASSES': (
+        #     'rest_framework.authentication.SessionAuthentication',
+        #     'rest_framework.authentication.TokenAuthentication',
+        # )
     }
